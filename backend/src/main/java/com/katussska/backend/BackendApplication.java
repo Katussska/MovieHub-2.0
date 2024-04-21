ca1bc454a8959969d056fff2baa25effc88ecb02
katussska <katkabaier@gmail.com>
2024-04-21 18:41:57 +0200

    film endpoints all working

--- backend/src/main/java/com/katussska/backend/BackendApplication.java
+++ backend/src/main/java/com/katussska/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.katussska.backend;
 
+import com.katussska.backend.service.GenreSer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +10,9 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
+
+        GenreSer genreService = context.getBean(GenreSer.class);
+        genreService.fetchAndSaveGenres();
     }
 
 }
